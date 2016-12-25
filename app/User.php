a71cb0d3a112e26c8496bf50503a31655f125f01
Amr Aly <amr.aly57@yahoo.com>
2016-12-25 21:26:58 +0200

    modified the user model and created 3 methods to get the user info

--- app/User.php
+++ app/User.php
@@ -26,4 +26,27 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function getName()
+    {
+        if($this->first_name && $this->last_name) {
+            return "{$this->first_name} {$this->last_name}";
+        }
+
+        if($this->first_name) {
+            return $this->first_name;
+        }
+
+        return null;
+    }
+
+    public function getNameOrUsername()
+    {
+        return $this->getName() ?: $this->username;
+    }
+
+    public function getFirstnameOrUsername()
+    {
+        return $this->first_name ? : $this->username;
+    }
 }
