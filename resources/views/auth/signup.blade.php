e6d298112d68b0845a1089cbb8dec7d9e7abf2db
Amr Aly <amr.aly57@yahoo.com>
2016-12-25 06:25:13 +0200

    wrote the markup for signup.blade

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,1 +1,23 @@
-signup
+@extends('templates.default')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-6">
+            <form action="" role="form" method="post" class="form-vertical">
+                <div class="form-group">
+                    <label for="email">Your email address</label>
+                    <input type="text" name="email" class="form-control" id="email" value="">
+                </div>
+                <div class="form-group">
+                    <label for="username">Choose a username</label>
+                    <input type="text" name="username" class="form-control" id="username" value="">
+                </div>
+                <div class="form-group">
+                    <label for="password">Choose a password</label>
+                    <input type="password" name="password" class="form-control" id="password" value="">
+                </div>
+                <button type="submit" class="btn btn-default">Signup</button>
+            </form>
+        </div>
+    </div>
+@endsection
