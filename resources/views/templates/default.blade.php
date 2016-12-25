bb820099932c410a1b811e00230bb7695d40da7d
Amr Aly <amr.aly57@yahoo.com>
2016-12-25 04:42:34 +0200

    wrote the markup for default.blade

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -3,8 +3,14 @@
     <head>
         <meta charset="UTF-8">
         <title>Social</title>
+        <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     </head>
     <body>
         
+        <div class="container">
+            @yield('content')
+        </div>
+
     </body>
 </html>
