5fbac9c4d7f5b09a77c20751d40fa1774398c1fe
Amr Aly <amr.aly57@yahoo.com>
2016-12-26 02:43:45 +0200

    added the search functionality

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use DB;
 
 class SearchController extends Controller
 {
-    public function getResults()
+    public function getResults(Request $request)
     {
-        return view('search.results');
+        $query = $request->input('query', 'default');
+        
+        if(!$query) {
+            return redirect()->route('home');
+        }
+
+        $users = User::where(DB::raw("CONCAT(first_name, ' ', last_name)"), 'like', "%{$query}%")
+        ->orWhere('username', 'like', "%{$query}%")
+        ->orWhere('email',  'like', "%{$query}%")
+        ->get();
+
+        dd($users);
+
+        return view('search.results')
+                ->withUsers($users);
     }
 }
