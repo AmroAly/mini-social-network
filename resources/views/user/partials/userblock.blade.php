d1ccb3a17cc9b6c239f77baee6b13fb340e8da84
Amr Aly <amr.aly57@yahoo.com>
2016-12-26 03:20:18 +0200

    added the url for the a tags to get the users

--- resources/views/user/partials/userblock.blade.php
+++ resources/views/user/partials/userblock.blade.php
@@ -1,11 +1,11 @@
 
 
 <div class="media">
-    <a href="" class="pull-left">
+    <a href="{{ route('profile.index', $user->username) }}" class="pull-left">
         <img src="{{ $user->getAvatarUrl() }}" alt="{{ $user->getNameOrUsername() }}" class="media-object">
     </a>
-    <a href="" class="pull-lef">
-       <h4 class="media-heading"><a href="">{{ $user->getNameOrUsername() }}</a></h4>
+    <a href="{{ route('profile.index', $user->username) }}" class="pull-lef">
+       <h4 class="media-heading"><a href="{{ route('profile.index', $user->username) }}">{{ $user->getNameOrUsername() }}</a></h4>
        @if($user->location)
         <p>{{ $user->location }}</p>
        @endif
