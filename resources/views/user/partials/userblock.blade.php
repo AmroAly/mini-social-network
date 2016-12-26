7bd9fd731fb7621d06e5bc87b1fa15e280fa7ced
Amr Aly <amr.aly57@yahoo.com>
2016-12-26 03:07:42 +0200

    added the getAvatar method and added the markup to userblock.blad

--- resources/views/user/partials/userblock.blade.php
+++ resources/views/user/partials/userblock.blade.php
@@ -1,10 +1,13 @@
 
 
 <div class="media">
-    <a href="" class="pull-lef">
-        <img src="" alt="" class="media-object">
+    <a href="" class="pull-left">
+        <img src="{{ $user->getAvatarUrl() }}" alt="{{ $user->getNameOrUsername() }}" class="media-object">
     </a>
     <a href="" class="pull-lef">
-       <h4 class="media-heading"><a href="">Amr</a></h4>
+       <h4 class="media-heading"><a href="">{{ $user->getNameOrUsername() }}</a></h4>
+       @if($user->location)
+        <p>{{ $user->location }}</p>
+       @endif
     </a>
 </div>
