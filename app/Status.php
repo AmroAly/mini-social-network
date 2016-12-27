9a878d0cfae0cd8b5a01bed50dccc53105ccd39d
Amr Aly <amr.aly57@yahoo.com>
2016-12-27 21:17:26 +0200

    added the relation in the user model

--- app/Status.php
+++ app/Status.php
@@ -3,12 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\User;
 class Status extends Model
 {
     protected $table = 'statuses';
 
     protected $fillable = ['body'];
 
-
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id');
+    }
 }
