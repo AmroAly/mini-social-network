27499098bde4fbd9bd7d869c29c82b70f54da020
Amr Aly <amr.aly57@yahoo.com>
2016-12-27 21:06:05 +0200

    added the validation

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -8,6 +8,8 @@ class StatusController extends Controller
 {
     public function postStatus(Request $request)
     {
-        
+        $this->validate($request, [
+            'status' => 'required|max:500'
+        ]);
     }
 }
--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -5,9 +5,14 @@
         <div class="col-md-6">
             <form action="{{ route('status.post') }}" method="post">
             {{ csrf_field() }}
-                <div class="form-group">
+                <div class="form-group{{ $errors->has('status') ? ' has-error':'' }}">
                     <textarea placeholder="What's up {{ Auth::user()->getFirstnameOrUsername() }}?" id="status" name="status" class="form-control" row="2"></textarea>
+                
+                    @if($errors->has('status'))
+                        <span class="help-block">{{ $errors->first('status') }}</span>
+                    @endif
                 </div>
+                
                 <button type="submit" class="btn btn-default">Add post</button>
             </form>
             <hr>
