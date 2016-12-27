78f0071682d5df34ab33e83a6c64b7ef965bbc1e
Amr Aly <amr.aly57@yahoo.com>
2016-12-27 23:05:28 +0200

    added the html markup, to display the statuses and added the links as well

--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -19,9 +19,41 @@
         </div>
     </div>
 
+<!--get the statuses for the current user and his friends -->
     <div class="row">
         <div class="col-md-5">
-        
+            @if(!$statuses->count())
+                <p>There is nothing in your timeline, yet.</p>
+            @else
+                @foreach($statuses as $status)
+                    
+                    <div class="media">
+                        <a href="{{ route('profile.index', $status->user->username) }}" class="pull-left">
+                            <img class="media-object" src="{{ $status->user->getAvatarUrl() }}" alt="{{ $status->user->getNameOrUsername() }}">
+                        </a>
+                        <div class="media-body">
+                            <h4 class="media-heading">
+                                <a href="{{ route('profile.index', $status->user->username) }}">{{ $status->user->getNameOrUsername() }}</a>
+                            </h4>
+                            <p>{{ $status->body }}</p>
+                            <ul class="list-inline">
+                                <li>{{ $status->created_at->diffForHumans() }}</li>
+                                <li><a href="">Like</a></li>
+                                <li>10 likes</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <textarea name="" id="" rows="1" class="form-control"></textarea>
+                        </div>
+                        <input type="submit" value="reply" class="btn btn-default btn-sm">
+                    </form>
+
+                @endforeach
+                {{ $statuses->links() }}
+            @endif
         </div>
     </div>
 @endsection
