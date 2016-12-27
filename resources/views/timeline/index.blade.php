9e8a28d7575b02298825db5d07503adca82d859d
Amr Aly <amr.aly57@yahoo.com>
2016-12-27 12:21:45 +0200

    added the html markup for timeline view

---
+++ resources/views/timeline/index.blade.php
@@ -0,0 +1,21 @@
+@extends('templates.default')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-6">
+            <form action="" method="post">
+                <div class="form-group">
+                    <textarea name="What's up name?" id="status" name="status" class="form-control" row="2"></textarea>
+                </div>
+                <button type="submit" class="btn btn-default">Add post</button>
+            </form>
+            <hr>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-5">
+        
+        </div>
+    </div>
+@endsection
