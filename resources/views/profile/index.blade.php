d2b03bdc70dec45782009a86dd82c5907f15261c
Amr Aly <amr.aly57@yahoo.com>
2016-12-27 07:29:22 +0200

    display the friends of the cuurent user in his profile

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -8,7 +8,15 @@
         </div>
 
         <div class="col-md-4 col-md-offset-2">
-        
+            <h3>{{ $user->getFirstnameOrUsername() }}'s friends</h3>
+
+            @if(!$user->friends()->count())
+                <p>{{ $user->getFirstnameOrUsername() }} has no friends yet.</p>
+            @else
+                @foreach($user->friends() as $user)
+                     @include('user.partials.userblock')
+                @endforeach
+            @endif
         </div>
         
     </div>
