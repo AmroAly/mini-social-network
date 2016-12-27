6644505cf264e751a3ec6e4d15b535c84fbdc755
Amr Aly <amr.aly57@yahoo.com>
2016-12-27 07:59:20 +0200

    display the friends in the friends.index view

--- resources/views/friends/index.blade.php
+++ resources/views/friends/index.blade.php
@@ -4,6 +4,14 @@
     <div class="rwo">
         <div class="col-md-6">
             <h3>Your Friends</h3>
+            @if(!$friends->count())
+                <p>You have no friends yet.</p>
+            @else
+                @foreach($friends as $user)
+                     @include('user.partials.userblock')
+                @endforeach
+            @endif
+
         </div>
         <div class="col-md-6">
             <h3>Friend Requests</h3>
