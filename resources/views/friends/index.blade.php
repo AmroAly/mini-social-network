be178dc8942e1c22f00291529e6e32ae60f6558f
Amr Aly <amr.aly57@yahoo.com>
2016-12-27 07:39:02 +0200

    created the friends view and its markup

---
+++ resources/views/friends/index.blade.php
@@ -0,0 +1,12 @@
+@extends('templates.default')
+
+@section('content')
+    <div class="rwo">
+        <div class="col-md-6">
+            <h3>Your Friends</h3>
+        </div>
+        <div class="col-md-6">
+            <h3>Friend Requests</h3>
+        </div>
+    </div>
+@endsection
