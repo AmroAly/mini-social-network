55dead33ebae75bb1a7d70311ec208e0979c4fad
Amr Aly <amr.aly57@yahoo.com>
2016-12-27 22:34:26 +0200

    get the statuses of the current user and his friends and pass them with the view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,13 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use App\Status;
 
 class HomeController extends Controller
 {
     public function index()
     {
         if(Auth::check()) {
-            return view('timeline.index');
+            $statuses = Status::where('user_id', Auth::user()->id)
+                ->orWhereIn('user_id', Auth::user()->friends()->pluck('id'))
+                ->orderBy('created_at', 'desc')
+                ->paginate(10);
+
+            return view('timeline.index')
+                    ->with('statuses', $statuses);
         }
         return view('home');
     }
